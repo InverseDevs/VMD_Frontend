1bef15a1bd8490cf040b63ff44a341f34b1a696e
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-04 13:21:58 +0300

    test config

--- src/profile/components/ProfilePost/ProfilePost.js
+++ src/profile/components/ProfilePost/ProfilePost.js
@@ -1,23 +1,23 @@
-// import React from 'react';
-// import './ProfilePost.css';
-// import like from '../../../images/like.svg';
-// class ProfilePost extends React.Component {
-//     render() {
-//         return(
-//             <div className="profile-post">
-//                 <div className="profile-post-header">
-//                     <div className="profile-post-header-left">
-//                         <button className="profile-post-like"><img className="post-like" src={like}/></button>
-//                         <button className="profile-post-comments">Комментарии</button>
-//                     </div>
-//                     <div className="user-img"></div>
-//                 </div>
-//                 <div className="post-img"></div>
-//                 <h6 className="post-text">Повседневная практика показывает, что новая модель организационной деятельности влечет за собой процесс внедрения и модернизации направлений прогрессивного развития.</h6>
-//                 <button className="post-readmore">Подробнее</button>
-//             </div>
-//         );
-//     }
-// }
-//
-// export default ProfilePost;
+import React from 'react';
+import './ProfilePost.css';
+import like from '../../../images/like.svg';
+class ProfilePost extends React.Component {
+    render() {
+        return(
+            <div className="profile-post">
+                <div className="profile-post-header">
+                    <div className="profile-post-header-left">
+                        <button className="profile-post-like"><img className="post-like" src={like}/></button>
+                        <button className="profile-post-comments">Комментарии</button>
+                    </div>
+                    <div className="user-img"></div>
+                </div>
+                <div className="post-img"></div>
+                <h6 className="post-text">Повседневная практика показывает, что новая модель организационной деятельности влечет за собой процесс внедрения и модернизации направлений прогрессивного развития.</h6>
+                <button className="post-readmore">Подробнее</button>
+            </div>
+        );
+    }
+}
+
+export default ProfilePost;
