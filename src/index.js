2530f3b53472592aa2feb04458abd9a5f2de6edb
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-04 12:07:37 +0300

    test config

--- src/index.js
+++ src/index.js
@@ -3,29 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import ChatApp from './lambda/chat/components/ChatApp/ChatApp';
 import ProfileApp from './profile/components/ProfileApp/ProfileApp';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <Route exact path="/chat" component={ChatApp}/>
+                <Route exact path="/" component={ProfileApp}/>
+            </BrowserRouter>
+        );
+    }
+}
 
-
-
-
-// class App extends React.Component{
-//     render(){
-//         return (
-//             <BrowserRouter>
-//                 <Route exact path="/chat" component={ChatApp}/>
-//                 <Route exact path="/" component={ProfileApp}/>
-//             </BrowserRouter>
-//         );
-//     }
-// }
-//
-// ReactDOM.render(
-//     <App/>,
-//     document.getElementById('root')
-// );
-// serviceWorker.unregister();
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+serviceWorker.unregister();
