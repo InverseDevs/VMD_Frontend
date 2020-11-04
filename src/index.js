9caa7fc94cbb1657a0396abde28222dc97ab2dce
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-04 11:16:09 +0300

    test config

--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// import ChatApp from './chat/components/ChatApp/ChatApp';
+import ChatApp from './chat/components/ChatApp/ChatApp';
 // import ProfileApp from './profile/components/ProfileApp/ProfileApp';
-import {BrowserRouter, Route} from 'react-router-dom';
+//import {BrowserRouter, Route} from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 
 
