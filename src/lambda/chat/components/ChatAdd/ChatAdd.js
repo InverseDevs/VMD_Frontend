ce972a668f2b2ece41a6eed8c8f7757aabab3143
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-04 15:07:09 +0300

    Update ChatAdd.js

--- src/lambda/chat/components/ChatAdd/ChatAdd.js
+++ src/lambda/chat/components/ChatAdd/ChatAdd.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ChatAdd.css';
-import addBtn from '../../../../../images/add_chat.png'
+import addBtn from '../../../../images/add_chat.png'
 const ChatAdd = ({addGroup}) => {
     return (
         <button onClick={addGroup} id="add-btn"><img src={addBtn}  id="add-btn-img"></img></button>
