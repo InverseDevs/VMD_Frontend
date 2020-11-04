4d6b45b88539058838a5d3702e2bb036d2b99aaf
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-04 19:29:46 +0300

    Update ChatApp.js

--- src/lambda/ChatApp.js
+++ src/lambda/ChatApp.js
@@ -7,7 +7,7 @@ class ChatApp extends React.Component{
     render(){
         return (
                     <div className="chat-page">
-                        <Link to="/"><img src={logo} id="chat-logo"/></Link>
+                        <Link to="/"><img src={require('../images/logo_opacity.png')} id="chat-logo"/></Link>
                         <div className="chat-big-ellipse" id="chat-big-1"></div>
                         <div className="chat-big-ellipse" id="chat-big-2"></div>
                         <div className="chat-ellipse" id="chat-1"></div>
