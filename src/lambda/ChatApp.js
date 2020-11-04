3c96733b9ee6f42db22b375ce04b5332a4bbab14
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-04 11:27:39 +0300

    test config

--- src/lambda/ChatApp.js
+++ src/lambda/ChatApp.js
@@ -5,17 +5,17 @@ import ChatContainer from './chat/components/ChatContainer/ChatContainer';
 import {BrowserRouter, Link} from 'react-router-dom';
 class ChatApp extends React.Component{
     render(){
-        return (
-                    <div className="chat-page">
-                        <Link to="/"><img src={logo} id="chat-logo"/></Link>
-                        <div className="chat-big-ellipse" id="chat-big-1"></div>
-                        <div className="chat-big-ellipse" id="chat-big-2"></div>
-                        <div className="chat-ellipse" id="chat-1"></div>
-                        <div className="chat-ellipse" id="chat-2"></div>
-                        <div className="chat-ellipse" id="chat-3"></div>
-                        <div className="chat-ellipse" id="chat-4"></div>
-                        <ChatContainer />
-                    </div>
+        return ("lol"
+                    // <div className="chat-page">
+                    //     <Link to="/"><img src={logo} id="chat-logo"/></Link>
+                    //     <div className="chat-big-ellipse" id="chat-big-1"></div>
+                    //     <div className="chat-big-ellipse" id="chat-big-2"></div>
+                    //     <div className="chat-ellipse" id="chat-1"></div>
+                    //     <div className="chat-ellipse" id="chat-2"></div>
+                    //     <div className="chat-ellipse" id="chat-3"></div>
+                    //     <div className="chat-ellipse" id="chat-4"></div>
+                    //     <ChatContainer />
+                    // </div>
         );
     }
 }
