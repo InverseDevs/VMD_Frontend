a29cf8c8c71bb03bb8d3b402cf9a372cfea8b2a5
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-20 16:54:33 +0500

    Update ProfilePostModal.js

--- src/profile/components/ProfilePostModal/ProfilePostModal.js
+++ src/profile/components/ProfilePostModal/ProfilePostModal.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './ProfilePostModal.css';
 const ProfilePostModal = ({ handleClose, show, children }) => {
     const showHideClassName = show ? "modal display-block" : "modal display-none";
