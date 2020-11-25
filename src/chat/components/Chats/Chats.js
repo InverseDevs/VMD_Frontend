bb4fe9d47693e695d68d19957c800943163e1560
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-26 00:56:38 +0500

    Update Chats.js

--- src/chat/components/Chats/Chats.js
+++ src/chat/components/Chats/Chats.js
@@ -6,22 +6,53 @@ import SimpleBarReact from 'simplebar-react';
 import 'simplebar/src/simplebar.css';
 import './Chats.css';
 
-const Chats = ({groups,addGroup,addTab}) => {
-    
+class Chats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            groups: []
+        }
+    }
+    getData = async (url) => {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization' : `${window.localStorage.getItem('token')}`
+            }
+
+        });
+        return await res.json();
+    } 
+    getChats = async () => {
+        await this.getData(`https://inversedevs.herokuapp.com/chats/${window.localStorage.getItem('id')}`)
+        .then(data => {
+            console.log(data);
+            this.setState({groups:data})
+        })
+       
+    }
+    renderGroups = (groups)=>{
+        return Object.values(groups).map((group,i) => <ChatGroup key={i} id={group.chat_id} users={group.users}/> );
+    }
+    componentDidMount() {
+        this.getChats();
+}
+    render(){
+    const groups = this.renderGroups(this.state.groups);
     return (
         
         <div className="chats">
             <SimpleBarReact style={{maxHeight: 700}}>
                 <div className="chat-group-container">
                     <div className="chat-group-scroll">
-                        {[...Array(groups)].map(()=> <ChatGroup addTab={addTab}/>)}
+                        {groups}
                     </div>
                 </div>
             </SimpleBarReact>
-            <ChatAdd addGroup={addGroup}/>
+            <ChatAdd addGroup={this.props.addGroup}/>
         </div>
     );
 }
-
+}
 
 export default Chats;
