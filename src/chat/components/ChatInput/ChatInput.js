36979ae24505b02a26b10da34b12b9024a3c3931
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-25 15:11:44 +0500

    Update ChatInput.js

--- src/chat/components/ChatInput/ChatInput.js
+++ src/chat/components/ChatInput/ChatInput.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './ChatInput.css';
 import SockJsClient from 'react-stomp';
-import chatAPI from './chatApi';
 
 const SOCKET_URL = 'https://inversedevs.herokuapp.com/websocket-chat';
 class ChatInput extends React.Component {
@@ -13,17 +12,11 @@ class ChatInput extends React.Component {
             name: ""
         }
     }
-    onInputChange = (e) => {
-        this.setState({message: e.target.value})
-    }
-    setName = (name) => {
-        console.log(name);
-        this.setState({name: name});
-    };
 
     sendMessage = () => {
+        let message = this.state.typedMessage;
         this.clientRef.sendMessage('/app/user-all', JSON.stringify({
-            message: this.state.typedMessage
+            message: message
         }));
     };
     onConnected = () => {
@@ -34,18 +27,19 @@ class ChatInput extends React.Component {
         console.log('New Message Received!!', msg);
         this.setMessages(this.state.messages.concat(msg));
       }
-    
-      onSendMessage = (msgText) => {
-        chatAPI.sendMessage(this.state.name, msgText).then(res => {
-          console.log('Sent', res);
-        }).catch(err => {
-          console.log('Error Occured while sending message to api');
-        })
-      }
+
     
     render()
 {    return(
         <div className="chat-input">
+             <SockJsClient
+              url={SOCKET_URL}
+              topics={['/topic/user']}
+              onConnect={this.onConnected}
+              onDisconnect={console.log("Disconnected!")}
+              onMessage={msg => this.onMessageReceived(msg)}
+              debug={false}
+            />
             <form className="chat-form" onSubmit={this.props.onMessageSubmit}>
                 <div className="chat-input-container">
                     <textarea type="text" placeholder="Type text here..." onChange={(event) => {
@@ -56,14 +50,7 @@ class ChatInput extends React.Component {
                     <input onClick={this.sendMessage} type="submit" className="msg-send" value=""/>
                 </div>
             </form>
-            <SockJsClient
-              url={SOCKET_URL}
-              topics={['/topic/user']}
-              onConnect={this.onConnected}
-              onDisconnect={console.log("Disconnected!")}
-              onMessage={msg => this.onMessageReceived(msg)}
-              debug={false}
-            />
+           
         </div>
     );}
 }
