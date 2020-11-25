35dd89aa1467cc719f89082329e3aa806024591a
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-25 15:24:35 +0500

    Update ChatInput.js

--- src/chat/components/ChatInput/ChatInput.js
+++ src/chat/components/ChatInput/ChatInput.js
@@ -23,7 +23,40 @@ class ChatInput extends React.Component {
         console.log("Connected!!")
       }
     
-      onMessageReceived = (msg) => {
+      onMessageReceived = (msg) => {import React from 'react';
+import './ChatInput.css';
+
+
+class ChatInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+            typedMessage: "",
+        }
+    }   
+    sendMsg = () => {
+        this.props.sendMessage(this.state.typedMessage);
+    }
+    render()
+{    return(
+        <div className="chat-input">
+            <form className="chat-form" onSubmit={this.props.onMessageSubmit}>
+                <div className="chat-input-container">
+                    <textarea type="text" placeholder="Type text here..." onChange={(event) => {
+                                               this.setState({typedMessage: event.target.value});
+                                           }} className="msg-input"/>
+                </div>
+                <div className="send-btn">
+                    <input onClick={this.sendMsg} type="submit" className="msg-send" value=""/>
+                </div>
+            </form>
+        </div>
+    );}
+}
+
+
+export default ChatInput;
         console.log('New Message Received!!', msg);
         this.setMessages(this.state.messages.concat(msg));
       }
