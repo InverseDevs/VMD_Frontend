2e439a13e7b08ea44b45deaf8764e353c8cca038
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-25 15:23:45 +0500

    Update ChatWindow.js

--- src/chat/components/ChatWindow/ChatWindow.js
+++ src/chat/components/ChatWindow/ChatWindow.js
@@ -3,16 +3,44 @@ import './ChatWindow.css';
 import TabList from '../TabList/TabList';
 import ChatInfo from '../ChatInfo/ChatInfo';
 import Chat from '../Chat/Chat';
+import SockJsClient from 'react-stomp';
 
-const ChatWindow = ({tabs,closeTab}) => {
+const SOCKET_URL = 'https://inversedevs.herokuapp.com/websocket-chat';
+
+class ChatWindow extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    onConnected = () => {
+        console.log("Connected!!")
+      }
+    
+      onMessageReceived = (msg) => {
+        console.log('New Message Received!!', msg);
+        this.setMessages(this.state.messages.concat(msg));
+      }
+      sendMessage = (msg) => {
+        this.clientRef.sendMessage('/app/user-all', JSON.stringify({
+            message: msg
+        }));
+    };
+    render(){
     return(
         <div className="chat-window">
-            <TabList tabs={tabs} closeTab={closeTab}/>
+            <SockJsClient
+              url={SOCKET_URL}
+              topics={['/topic/user']}
+              onConnect={this.onConnected}
+              onDisconnect={console.log("Disconnected!")}
+              onMessage={msg => this.onMessageReceived(msg)}
+              debug={false}
+            />
+            <TabList tabs={this.props.tabs} closeTab={this.props.closeTab}/>
             <ChatInfo />
-            <Chat />
+            <Chat sendMessage={this.sendMessage}/>
         </div>
     );
 }
-
+}
 
 export default ChatWindow;
