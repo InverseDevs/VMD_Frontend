4a38c43cc18e519f81869badb3dc5af4ed2e6df2
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-25 15:27:04 +0500

    Update ChatInput.js

--- src/chat/components/ChatInput/ChatInput.js
+++ src/chat/components/ChatInput/ChatInput.js
@@ -1,30 +1,5 @@
 import React from 'react';
 import './ChatInput.css';
-import SockJsClient from 'react-stomp';
-
-const SOCKET_URL = 'https://inversedevs.herokuapp.com/websocket-chat';
-class ChatInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-            typedMessage: "",
-            name: ""
-        }
-    }
-
-    sendMessage = () => {
-        let message = this.state.typedMessage;
-        this.clientRef.sendMessage('/app/user-all', JSON.stringify({
-            message: message
-        }));
-    };
-    onConnected = () => {
-        console.log("Connected!!")
-      }
-    
-      onMessageReceived = (msg) => {import React from 'react';
-import './ChatInput.css';
 
 
 class ChatInput extends React.Component {
@@ -51,39 +26,6 @@ class ChatInput extends React.Component {
                     <input onClick={this.sendMsg} type="submit" className="msg-send" value=""/>
                 </div>
             </form>
-        </div>
-    );}
-}
-
-
-export default ChatInput;
-        console.log('New Message Received!!', msg);
-        this.setMessages(this.state.messages.concat(msg));
-      }
-
-    
-    render()
-{    return(
-        <div className="chat-input">
-             <SockJsClient
-              url={SOCKET_URL}
-              topics={['/topic/user']}
-              onConnect={this.onConnected}
-              onDisconnect={console.log("Disconnected!")}
-              onMessage={msg => this.onMessageReceived(msg)}
-              debug={false}
-            />
-            <form className="chat-form" onSubmit={this.props.onMessageSubmit}>
-                <div className="chat-input-container">
-                    <textarea type="text" placeholder="Type text here..." onChange={(event) => {
-                                               this.setState({typedMessage: event.target.value});
-                                           }} className="msg-input"/>
-                </div>
-                <div className="send-btn">
-                    <input onClick={this.sendMessage} type="submit" className="msg-send" value=""/>
-                </div>
-            </form>
-           
         </div>
     );}
 }
