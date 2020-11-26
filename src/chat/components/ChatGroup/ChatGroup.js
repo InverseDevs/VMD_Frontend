7f3af8074e12847b3ff3d30e06e89e6459c6214c
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-27 00:20:12 +0500

    Update ChatGroup.js

--- src/chat/components/ChatGroup/ChatGroup.js
+++ src/chat/components/ChatGroup/ChatGroup.js
@@ -4,21 +4,20 @@ import './ChatGroup.css';
 class ChatGroup extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {user: null};
     }
     extractUser = (users) => {
             let user = Object.values(users).map(user => user.id != window.localStorage.getItem('id') ? user : null);
             user = user.filter(user => user != null);
             if (user.length == 1)
             {   
-                this.setState({user: {...user}})
                 return {...user}
 
             }
         
     }
     makeInfo = () => {
-        this.props.getInfo(this.state.user[0].name,this.state.user[0].avatar, this.props.id)
+        let user = this.extractUser(this.props.users);
+        this.props.getInfo(user[0].name,user[0].avatar, this.props.id)
     }
     render(){
     let user = this.extractUser(this.props.users);
