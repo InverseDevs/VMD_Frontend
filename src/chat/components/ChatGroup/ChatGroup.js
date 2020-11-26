4f0c40ec77f68aa685adc796b668f0754dd4e331
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-27 00:06:53 +0500

    chatInfo

--- src/chat/components/ChatGroup/ChatGroup.js
+++ src/chat/components/ChatGroup/ChatGroup.js
@@ -11,16 +11,19 @@ class ChatGroup extends React.Component {
             user = user.filter(user => user != null);
             if (user.length == 1)
             {   
-                
+                this.setState({user: {...user}})
                 return {...user}
 
             }
         
     }
+    makeInfo = () => {
+        this.props.getInfo(this.state.user[0].name,this.state.user[0].avatar, this.props.id)
+    }
     render(){
     let user = this.extractUser(this.props.users);
     return(
-        <div className="chat-group">
+        <div className="chat-group" onClick={this.props.makeInfo}>
             <div onClick={this.props.addTab} className={user[0].avatar != '' ? 'group-image-exist' : 'group-image'}>{user[0].avatar != '' ? <img className="user-pic" src={user[0].avatar} alt="user-img"/> : null}</div>
             <p className="group-name">{user[0].name}</p>
         </div>
--- src/chat/components/ChatInfo/ChatInfo.js
+++ src/chat/components/ChatInfo/ChatInfo.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './ChatInfo.css';
 
 class ChatInfo extends React.Component{
+    constructor(props) {
+        super(props);
+    }
     render(){
         return (
             <div className="chat-info">
-                <div className="chat-img"></div>
-                <p className="chat-info-name">Chat Name</p>
+                <div className={this.props.chatInfo.img != '' ? "chat-img-exist" : "chat-img"}>{this.props.chatInfo.img != ''? <img src={this.props.chatInfo.img} alt="chat-img" className="chat-img-icon"/> : null}</div>
+                <p className="chat-info-name">{this.props.chatInfo.name != '' ? this.props.chatInfo.name : 'chatName'}</p>
             </div>
         )
     }
