79220697ed486537a7b91b0fec3f9822e6ac7b5e
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-26 22:00:56 +0500

    Update ChatGroup.js

--- src/chat/components/ChatGroup/ChatGroup.js
+++ src/chat/components/ChatGroup/ChatGroup.js
@@ -8,21 +8,16 @@ class ChatGroup extends React.Component {
     }
     extractUser = (users) => {
         console.log(users)
-        let user = users.find(user => {user.id != window.localStorage.getItem('id')})
-        console.log("this is user",user)
-        this.setState({user: user});
-
     }
     componentDidMount() {
         this.extractUser(this.props.users);
-        console.log(this.state.user);
     }
     render(){
     
     return(
         <div className="chat-group">
-            <div onClick={this.props.addTab} className="group-image"><img src={this.state.user.avatar != null ? this.state.user.avatar: null} alt="user-pic"/></div>
-            <p className="group-name">{this.state.user.name}</p>
+            <div onClick={this.props.addTab} className="group-image"></div>
+            <p className="group-name"></p>
         </div>
     );
 }
