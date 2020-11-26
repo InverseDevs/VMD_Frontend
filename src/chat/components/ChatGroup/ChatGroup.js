ed0121ac9f3b7d8a889fc809f1a18c05d5ab40e9
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-26 21:35:02 +0500

    Update ChatGroup.js

--- src/chat/components/ChatGroup/ChatGroup.js
+++ src/chat/components/ChatGroup/ChatGroup.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './ChatGroup.css';
 
-const ChatGroup = ({addTab}) => {
+class ChatGroup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {user: null};
+    }
+    extractUser = (users) => {
+        let user = users.find(user => user.id == window.localStorage.getItem('id'))
+        this.setState({user: user});
+    }
+    componentDidMount() {
+        this.extractUser(this.props.users);
+    }
+    render(){
+
     return(
         <div className="chat-group">
-            <div onClick={addTab} className="group-image"></div>
-            <p className="group-name">Group Name</p>
+            <div onClick={this.props.addTab} className="group-image"><img src={this.state.user.avatar} alt="user-pic"/></div>
+            <p className="group-name">{this.state.user.name}</p>
         </div>
     );
 }
-
+}
 export default ChatGroup;
