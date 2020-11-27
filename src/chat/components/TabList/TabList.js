288436460e16712c28a3fc1288be5a464b7d8be5
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-27 14:34:55 +0500

    Update TabList.js

--- src/chat/components/TabList/TabList.js
+++ src/chat/components/TabList/TabList.js
@@ -4,13 +4,12 @@ import Tab from '../Tab/Tab';
 class TabList extends React.Component {
     constructor(props){
         super(props);
-        this.state={tabs : this.props.tabs}
     }
     renderTabs = (tabs) => {
         return Object.values(tabs).map((tab,i) => <Tab key={i} closeTab={this.props.closeTab} tabInfo={tab.info} />)
     }
     render(){
-    const tabs = this.renderTabs(this.state.tabs);
+    const tabs = this.renderTabs(this.props.tabs);
     return (
         <div className="tab-list">
             {tabs}
