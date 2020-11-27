eefec26c982d20de3aadc089fb4e6628ce434b91
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-27 14:08:31 +0500

    Update ChatGroup.js

--- src/chat/components/ChatGroup/ChatGroup.js
+++ src/chat/components/ChatGroup/ChatGroup.js
@@ -24,7 +24,7 @@ class ChatGroup extends React.Component {
     let user = this.extractUser(this.props.users);
     return(
         <div className="chat-group" onClick={this.makeInfo}>
-            <div onClick={this.props.addTab} className={user[0].avatar != '' ? 'group-image-exist' : 'group-image'}>{user[0].avatar != '' ? <img className="user-pic" src={user[0].avatar} alt="user-img"/> : null}</div>
+            <div className={user[0].avatar != '' ? 'group-image-exist' : 'group-image'}>{user[0].avatar != '' ? <img className="user-pic" src={user[0].avatar} alt="user-img"/> : null}</div>
             <p className="group-name">{user[0].name}</p>
         </div>
     );
