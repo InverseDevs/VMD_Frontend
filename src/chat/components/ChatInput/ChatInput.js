f1dddf3d037416d495b9e2a2df7a0dd2e686a2de
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-27 17:18:05 +0500

    Update ChatInput.js

--- src/chat/components/ChatInput/ChatInput.js
+++ src/chat/components/ChatInput/ChatInput.js
@@ -13,14 +13,15 @@ class ChatInput extends React.Component {
     sendMsg = (e) => {
         e.preventDefault();
         this.props.sendMessage(this.state.typedMessage);
-        this.setState({typedMessage: ''});
+        this.setState({typedMessage: ''})
+        document.getElementById('message-are').value = '';
     }
     render()
 {    return(
         <div className="chat-input">
             <form className="chat-form" onSubmit={this.props.onMessageSubmit}>
                 <div className="chat-input-container">
-                    <textarea type="text" placeholder="Type text here..." onChange={(event) => {
+                    <textarea type="text" id="message-area" placeholder="Type text here..." onChange={(event) => {
                                                this.setState({typedMessage: event.target.value});
                                            }} className="msg-input"/>
                 </div>
