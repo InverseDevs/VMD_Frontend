7dfefdcf9f87bae71605c6e560085c11b8febd3e
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-28 01:29:59 +0500

    Update TabList.js

--- src/chat/components/TabList/TabList.js
+++ src/chat/components/TabList/TabList.js
@@ -9,9 +9,8 @@ class TabList extends React.Component {
         return Object.values(tabs).map((tab,i) => <Tab key={i} getMessages={this.props.getMessages} getInfo={this.props.getInfo} chatInfo={this.props.chatInfo} closeTab={this.props.closeTab} name={tab.name} avatar={tab.avatar} id={tab.id}/>)
     }
     render(){
-    console.log(this.props.tabs)
-    console.log(JSON.parse(window.localStorage.getItem('tabs')).map((tab,i) => <Tab key={i} getMessages={this.props.getMessages} getInfo={this.props.getInfo} chatInfo={this.props.chatInfo} closeTab={this.props.closeTab} name={tab.name} avatar={tab.avatar} id={tab.id}/>))
-    const tabs = this.props.tabs ? this.renderTabs(this.props.tabs) :JSON.parse(window.localStorage.getItem('tabs')).map((tab,i) => <Tab key={i} getMessages={this.props.getMessages} getInfo={this.props.getInfo} chatInfo={this.props.chatInfo} closeTab={this.props.closeTab} name={tab.name} avatar={tab.avatar} id={tab.id}/>);
+  
+    const tabs = this.props.tabs != [] ? this.renderTabs(this.props.tabs) :JSON.parse(window.localStorage.getItem('tabs')).map((tab,i) => <Tab key={i} getMessages={this.props.getMessages} getInfo={this.props.getInfo} chatInfo={this.props.chatInfo} closeTab={this.props.closeTab} name={tab.name} avatar={tab.avatar} id={tab.id}/>);
     return (
         <div className="tab-list">
             {tabs}
