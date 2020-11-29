494de6c8f3b7aa8b42bb3de0aa75cdbf3ec1082e
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-30 00:16:27 +0500

    Update Chat.js

--- src/chat/components/Chat/Chat.js
+++ src/chat/components/Chat/Chat.js
@@ -69,8 +69,13 @@ class Chat extends React.Component {
         return messages.map(message => message.sender_id == window.localStorage.getItem('id') ? <MessageTo message={message.message} sender_id={message.sender_id} sent_time={message.sent_time}/> : <MessageFrom message={message.message} sender_id={message.sender_id} sent_time={message.sent_time}/> )
     }
     render(){
-         console.log('state',[...Object.values(this.state.messages)[0]])
+         console.log('state',Object.values(this.state.messages)[0])
          const messages = null
+         const mes = []
+         for (let i in Object.values(this.state.messages)[0]){
+            mes.push(Object.values(this.state.messages)[0][i])                  
+         }
+         console.log('messages',mes)
         //const messages = this.state.messages != [] ? this.props.messages != null ? this.renderMessages(Object.values(this.state.messages)[0].concat(this.props.messages[0])) : this.renderMessages(Object.values(this.state.messages)[0]) : null;
             return(
             
