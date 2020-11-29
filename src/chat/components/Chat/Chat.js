69a6de892d7060de88772112ccd9690638af9771
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-30 01:08:35 +0500

    Update Chat.js

--- src/chat/components/Chat/Chat.js
+++ src/chat/components/Chat/Chat.js
@@ -32,6 +32,9 @@ class Chat extends React.Component {
         await this.getData(`https://inversedevs.herokuapp.com/chat/messages/${this.props.chatId}`, {first_idx : this.state.first_idx, last_idx: this.state.second_idx})
         .then(data => {
             if (data.status != 'no messages'){
+                console.log('state',this.state.messages)
+                console.log('new',data.messages)
+                console.log('concat',this.state.messages.concat(data.messages))
                 messages = messages.concat(data.messages)
                 this.setState({first_idx: this.state.first_idx + 10})
                 this.setState({second_idx: this.state.second_idx + 10})
