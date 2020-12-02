73196694f7e7b5556a2371a4e402e3dc1c56bca5
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-03 01:38:26 +0500

    Update GroupsHeader.js

--- src/groups/components/GroupsHeader/GroupsHeader.js
+++ src/groups/components/GroupsHeader/GroupsHeader.js
@@ -129,7 +129,7 @@ return Object.values(new_members).map((member,idx) => <GroupParticipants key={id
                     <div className="group-header-name">
                         {this.props.name}
                     </div>
-                    <div className={this.props.avatar != ''? "groups-image-exists" : "groups-image"}>
+                    <div className={this.props.avatar != ''? "groups-header-image-exists" : "groups-header-image"}>
                             {this.props.avatar != ''? <img src={this.props.avatar} className="group-avatar" alt="group-avatar"/> : null}
                     </div>
                     <div className="group-buttons">
