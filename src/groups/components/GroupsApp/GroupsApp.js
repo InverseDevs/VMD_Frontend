9db449fab7059797fed04ba9b607ef6a3f707b77
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-02 18:10:03 +0500

    Update GroupsApp.js

--- src/groups/components/GroupsApp/GroupsApp.js
+++ src/groups/components/GroupsApp/GroupsApp.js
@@ -15,7 +15,7 @@ class GroupsApp extends React.Component {
     componentDidMount(){
         console.log(window.location.pathname)
             if (window.location.pathname != '/groups'){
-                console.log(window.location.pathname.slice(6));
+                console.log(window.location.pathname.slice(8));
             }
     }
     render() { 
