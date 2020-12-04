404ae79759dcf4b33886563d1beec614436c7f2c
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-04 22:08:14 +0500

    Update GroupBanned.js

--- src/groups/components/GroupBanned/GroupBanned.js
+++ src/groups/components/GroupBanned/GroupBanned.js
@@ -6,16 +6,16 @@ class GroupBanned extends Component {
         super(props);
     }
     render() { 
-        //const status = this.props.online === false || this.props.online === "false" ? <div className="friend-offline"></div> : <div className="friend-online"></div>
+        const status = this.props.online === false || this.props.online === "false" ? <div className="friend-offline"></div> : <div className="friend-online"></div>
         return (
             
 				<div className="friend">
                     <div className="friend-info">
-                        <div className="ava"></div>
+                        <div className="ava">{this.props.avatar != '' ? <img src={this.props.avatar} className="friend-avatar"/> : null }</div>
                         <div className="ava-devisor">
                         <div className="friend-name">
-                            Name
-                            {/* {status} */}
+	      		    {this.props.name}
+                            {status} 
                         </div>
                     </div>
                     <button  type="button" className="delete-friend">Разбанить</button>
