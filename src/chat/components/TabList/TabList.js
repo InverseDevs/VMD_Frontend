e4bff941dadaee89e2f8f5ed7abc8f3fa34580ce
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-05 16:46:54 +0500

    Update TabList.js

--- src/chat/components/TabList/TabList.js
+++ src/chat/components/TabList/TabList.js
@@ -5,7 +5,7 @@ class TabList extends React.Component {
     constructor(props){
         super(props);
     }
-    renderTabs = (tabs,propsTabs) => {
+    renderTabs = (tabs) => {
         let found = false;
         let new_tabs = []
         new_tabs.push(tabs[0])
@@ -23,30 +23,13 @@ class TabList extends React.Component {
                new_tabs.push(tabs[i])
             }
         }
-        if (propsTabs != []){
-            found = false;
-            for (let i =0; i < propsTabs.length;++i){
-                found =false
-                for (let j = 0; j < new_tabs.length;++j){
-                    if (new_tabs[j].id == propsTabs[i].id){
-                        found = true;   
-                    }
-                }
-                if (found == true){
-                    continue;   
-                }
-                else{
-                    new_tabs.push(propsTabs[i])   
-                }
-            }
-        }
+      
         console.log('new',new_tabs)
-        console.log('came',propsTabs);
         console.log('old',tabs)
         return new_tabs[0] != undefined ? Object.values(new_tabs).map((tab,i) => <Tab key={i} getMessages={this.props.getMessages} getInfo={this.props.getInfo} chatInfo={this.props.chatInfo} closeTab={this.props.closeTab} name={tab.name} avatar={tab.avatar} id={tab.id}/>) : null
     }
     render(){
-    const tabs = this.props.tabs != [] ? window.localStorage.getItem('tabs') != "" ?  this.renderTabs(JSON.parse(window.localStorage.getItem('tabs')),this.props.tabs) : null : window.localStorage.getItem('tabs') != "" ? this.renderTabs(JSON.parse(window.localStorage.getItem('tabs'))) : null;
+    const tabs = this.props.tabs != [] ? window.localStorage.getItem('tabs') != "" ?  this.renderTabs(JSON.parse(window.localStorage.getItem('tabs')).concat(this.props.tabs)) : null : window.localStorage.getItem('tabs') != "" ? this.renderTabs(JSON.parse(window.localStorage.getItem('tabs'))) : null;
     return (
         <div className="tab-list">
             {tabs}
