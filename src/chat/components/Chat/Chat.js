d0598e312de2c08b2afe672bec4210160b370de9
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-08 01:57:23 +0500

    Update Chat.js

--- src/chat/components/Chat/Chat.js
+++ src/chat/components/Chat/Chat.js
@@ -44,6 +44,7 @@ class Chat extends React.Component {
                 if (this.state.firstLoad == true){
                     this.setState({first_idx: this.state.first_idx + 6})
                     this.setState({second_idx: this.state.second_idx + 6})
+                    document.getElementById('slider-container').scrollTop = 120;
                 }
                 this.setState({firstLoad: true})
             }
