284ac74512f203165457e0a293ad39fd6c6613ca
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-08 02:07:14 +0500

    Update Chat.js

--- src/chat/components/Chat/Chat.js
+++ src/chat/components/Chat/Chat.js
@@ -44,7 +44,6 @@ class Chat extends React.Component {
                 if (this.state.firstLoad == true){
                     this.setState({first_idx: this.state.first_idx + 6})
                     this.setState({second_idx: this.state.second_idx + 6})
-                    document.getElementById('slider-container').scrollTop = 150;
                 }
                 this.setState({firstLoad: true})
             }
@@ -57,7 +56,7 @@ class Chat extends React.Component {
         event.preventDefault();
     }
     onScrollBarChange = () => {
-        if (document.getElementById('slider-container').scrollTop == 100){
+        if (document.getElementById('slider-container').scrollTop == 60){
             this.getMessages();
         }
     }
