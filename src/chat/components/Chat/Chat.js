e3f9974ba4c8e3abe61395f4fbfd4a409734c950
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-08 23:36:46 +0500

    Update Chat.js

--- src/chat/components/Chat/Chat.js
+++ src/chat/components/Chat/Chat.js
@@ -63,6 +63,9 @@ class Chat extends React.Component {
             this.getMessages();
             document.getElementById('slider-container').scrollTop = 200;
         }
+        if(document.getElementById('slider-container').scrollHeight - document.getElementById('slider-container').scrollTop == document.getElementById('slider-container').clientHeight){
+            document.getElementById('slider-container').scrollTop = document.getElementById('slider-container').clientHeight;   
+        }
   
     }
 
