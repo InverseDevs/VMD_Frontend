1091471a27ae97da4ded8b1ce29b94532c140b7b
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-10 01:05:02 +0500

    Update ProfilePost.js

--- src/profile/components/ProfilePost/ProfilePost.js
+++ src/profile/components/ProfilePost/ProfilePost.js
@@ -73,7 +73,7 @@ class ProfilePost extends React.Component {
                         <button onClick={this.likePressed} className="profile-post-like"><img className="post-like" src={ this.checkLike(this.props.likes) === false ? like : likeClicked}/></button>
                         <button onClick={this.showCommentsModal} className="profile-post-comments">Комментарии</button>
                         <button className="profile-post-comments" onClick={this.deletePost}>Удалить</button>
-                        <div className="friend-name">
+                        <div className="friend-name-link">
                          <Link to={`/profile/${this.props.senderId}`}><div className="profile-post-author">{this.props.name}</div></Link>
                         </div> 
                         <ProfileCommentsModal show={this.state.showComments} handleClose={this.hideCommentsModal}>
