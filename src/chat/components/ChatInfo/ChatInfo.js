d643ec9caf1d899d7ff4df285387968f3322e39d
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-12-11 01:00:19 +0500

    Update ChatInfo.js

--- src/chat/components/ChatInfo/ChatInfo.js
+++ src/chat/components/ChatInfo/ChatInfo.js
@@ -23,9 +23,13 @@ class ChatInfo extends React.Component{
         if (this.props.chatInfo != null) {
         return (
             <div className="chat-info">
-                <Link to={`/profile/${this.props.id}`} >{this.props.chatInfo.img != ''? <img src={this.props.chatInfo.img} alt="chat-img" className="chat-img-icon"/> : <div className="chat-img"></div>}</Link> 
+            {this.props.id != -1 ? <Link to={`/profile/${this.props.id}`} >
+{this.props.chatInfo.img != ''? <img src={this.props.chatInfo.img} alt="chat-img" className="chat-img-icon"/> : 
+<div className="chat-img"></div>}</Link> : {this.props.chatInfo.img != ''? <img src={this.props.chatInfo.img} alt="chat-img" className="chat-img-icon"/>}
                 <div className="friend-name-link">  
-                <Link to={`/profile/${this.props.id}`}><p className="chat-info-name">{this.props.chatInfo.name != '' ? this.props.chatInfo.name : 'chatName'}</p></Link>
+                   {this.props.id != -1 ? <Link to={`/profile/${this.props.id}`}>
+                        <p className="chat-info-name">{this.props.chatInfo.name != '' ? this.props.chatInfo.name : 'chatName'}</p>
+                    </Link> :  <p className="chat-info-name">{this.props.chatInfo.name != '' ? this.props.chatInfo.name : 'chatName'}</p>}
                 </div>
                 <button onClick={this.deleteChat} className="delete-chat">Удалить</button>
             </div>
